f088c1aa1651ad375456187347b9c32dcbcf18b6
Esteban Maya <juanestebanmayasanchez@gmail.com>
2025-11-17 00:45:05 -0500

    Primer commit limpio

---
+++ MenuPrincipalForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ProyectFinalBD
+{
+    partial class MenuPrincipalForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Panel panelContenido;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(30, 58, 138);
+            this.panelMenu.Controls.Add(this.btnSalir);
+            this.panelMenu.Controls.Add(this.btnReportes);
+            this.panelMenu.Controls.Add(this.btnVentas);
+            this.panelMenu.Controls.Add(this.btnProductos);
+            this.panelMenu.Controls.Add(this.btnClientes);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 450);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.FlatAppearance.BorderSize = 0;
+            this.btnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClientes.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnClientes.ForeColor = System.Drawing.Color.White;
+            this.btnClientes.Location = new System.Drawing.Point(0, 30);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(200, 40);
+            this.btnClientes.TabIndex = 0;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.FlatAppearance.BorderSize = 0;
+            this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductos.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnProductos.ForeColor = System.Drawing.Color.White;
+            this.btnProductos.Location = new System.Drawing.Point(0, 80);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(200, 40);
+            this.btnProductos.TabIndex = 1;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.FlatAppearance.BorderSize = 0;
+            this.btnVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVentas.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnVentas.ForeColor = System.Drawing.Color.White;
+            this.btnVentas.Location = new System.Drawing.Point(0, 130);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(200, 40);
+            this.btnVentas.TabIndex = 2;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            this.btnVentas.Click += new System.EventHandler(this.btnVentas_Click);
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnReportes.ForeColor = System.Drawing.Color.White;
+            this.btnReportes.Location = new System.Drawing.Point(0, 180);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(200, 40);
+            this.btnReportes.TabIndex = 3;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.UseVisualStyleBackColor = true;
+            this.btnReportes.Click += new System.EventHandler(this.btnReportes_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnSalir.ForeColor = System.Drawing.Color.White;
+            this.btnSalir.Location = new System.Drawing.Point(0, 230);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(200, 40);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // panelContenido
+            // 
+            this.panelContenido.BackColor = System.Drawing.Color.White;
+            this.panelContenido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenido.Location = new System.Drawing.Point(200, 0);
+            this.panelContenido.Name = "panelContenido";
+            this.panelContenido.Size = new System.Drawing.Size(600, 450);
+            this.panelContenido.TabIndex = 1;
+            // 
+            // MenuPrincipalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelContenido);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "MenuPrincipalForm";
+            this.Text = "Menú Principal";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ MenuPrincipalForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+using ProyectFinalBD.Entities;
+using ProyectFinalBD.Transactions;
+using ProyectFinalBD.Reports;
+using ProyectFinalBD.Utils;
+
+namespace ProyectFinalBD
+{
+    public partial class MenuPrincipalForm : Form
+    {
+        private string usuarioActual;
+        private int nivelUsuario;
+
+        public MenuPrincipalForm(string usuario = "admin", int nivel = 1)
+        {
+            usuarioActual = usuario;
+            nivelUsuario = nivel;
+
+            InitializeComponent();
+            ConfigurarMenu();
+        }
+
+        private void ConfigurarMenu()
+        {
+            // Ajustar permisos según niveles
+
+            // Nivel 3: solo consultas
+            if (nivelUsuario == 3)
+            {
+                btnClientes.Enabled = false;
+                btnProductos.Enabled = false;
+                btnVentas.Enabled = false;
+                btnReportes.Enabled = false;
+            }
+
+            // Nivel 2: no puede abrir usuarios ni bitácora
+            if (nivelUsuario == 2)
+            {
+                // Aquí luego deshabilitas botones si los agregas
+            }
+        }
+
+        // ==============================
+        // MANEJO DE SUBFORMULARIOS
+        // ==============================
+
+        private void AbrirFormulario(Form frm)
+        {
+            panelContenido.Controls.Clear();
+            frm.TopLevel = false;
+            frm.Dock = DockStyle.Fill;
+            panelContenido.Controls.Add(frm);
+            frm.Show();
+        }
+
+        private void btnClientes_Click(object sender, EventArgs e)
+            => AbrirFormulario(new ClienteListForm());
+
+        private void btnProductos_Click(object sender, EventArgs e)
+            => AbrirFormulario(new ProductoListForm());
+
+        private void btnVentas_Click(object sender, EventArgs e)
+            => AbrirFormulario(new VentaForm());
+
+        private void btnReportes_Click(object sender, EventArgs e)
+            => AbrirFormulario(new ReportVentasMesForm());
+
+        private void btnSalir_Click(object sender, EventArgs e)
+            => this.Close();
+    }
+}
